a2b81079931cd8f14b6d677447357977fa76b279
Maxim Asafatov <maxim.asafatov@gmail.com>
2013-08-24 20:24:54 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thisis::Application.config.secret_key_base = '2589a5fc6ceaaa0f8d9f8785db628c43f9cfc5605350c27287969d87fdf3f0cf4b3f2cde3c397ae042558c5fe7f7e955af173a3358a8d492a89b2097fd5413ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Thisis::Application.config.session_store :cookie_store, key: '_thisis_session'
